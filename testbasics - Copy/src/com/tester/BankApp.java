23c23af9b67c888b735460316c2223e15283f8c0
RahulSinghhub <p1273105@gmial.com>
2024-05-07 18:29:51 +0530

    pushing on 7

---
+++ testbasics - Copy/src/com/tester/BankApp.java
@@ -0,0 +1,8 @@
+package com.tester;
+import com.banking.BankAccount;
+public class BankApp{
+   public static void main(String[] args){
+       BankAccount b1 = new BankAccount(1,"w","e",2019);
+        System.out.println(b1);   
+}
+}
---
+++ testbasics/src/BankApp.java
@@ -0,0 +1,6 @@
+public class BankApp{
+   public static void main(String[] args){
+       BankAccount b1 = new BankAccount(1,"w","e",2019);
+        System.out.println(b1);   
+}
+}
